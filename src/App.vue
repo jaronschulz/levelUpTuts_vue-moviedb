e2a678a1c30f42865142ec4c94a2441dbaeed4d5
Jaron Schulz <jaronschulz@gmail.comm>
2018-06-27 20:24:52 +0200

    v18 - installing vue-router and setting it up

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <div id="app" v-if="status === 'Ready'">
+  <div id="app">
     <Header :title="title"/>
-    <MoviesList />
+    <router-view />
   </div>
-  <div v-else-if="status === 'Loading'">
-    Loading
-  </div>
-  <div v-else>
-    Error
-  </div>
+
 </template>
 
 <script>
 import Header from "./components/Header.vue";
-import MoviesList from "./components/MoviesList.vue";
 
 export default {
   name: "app",
   data() {
     return {
-      title: "Vue Movie DB",
-      status: "Ready"
+      title: "Vue Movie DB"
     };
   },
   components: {
-    MoviesList,
     Header
   }
 };
